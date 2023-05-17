3ca45a35cd3c50908d3951dbac278e7e9758f595
Omar Saeed <omar.ahmed.saeed.1999@gmail.com>
2023-05-17 04:03:20 +0300

    fix: add description to task json schema

--- src/api/task/task.validation.ts
+++ src/api/task/task.validation.ts
@@ -1,13 +1,41 @@
 const taskSchema = {
+  $schema: "http://json-schema.org/draft-07/schema#",
+  title: "Task",
   type: "object",
   properties: {
-    name: { type: "string" },
-    deadlineDate: { type: "string" },
-    employeeId: { type: "integer" },
-    finishedAt: { type: ["string", "null"] },
-    taskStatus: { type: "string", enum: ["PENDING", "IN_PROGRESS", "COMPLETED"] },
-    taskSalary: { type: "number" },
-    isPaid: { type: "boolean", default: false },
+    name: {
+      type: "string",
+      description: "The name of the task.",
+    },
+    deadlineDate: {
+      type: "string",
+      format: "date-time",
+      description: "The deadline date of the task.",
+    },
+    employeeId: {
+      type: "integer",
+      description: "The ID of the employee assigned to the task.",
+    },
+    finishedAt: {
+      type: ["string", "null"],
+      format: "date-time",
+      nullable: true,
+      description: "The date and time when the task was finished.",
+    },
+    taskStatus: {
+      type: "string",
+      enum: ["PENDING", "IN_PROGRESS", "COMPLETED"],
+      description: "The status of the task.",
+    },
+    taskSalary: {
+      type: "number",
+      description: "The salary associated with the task.",
+    },
+    isPaid: {
+      type: "boolean",
+      default: false,
+      description: "Indicates whether the task has been paid.",
+    },
   },
   required: ["name", "deadlineDate", "employeeId", "taskStatus", "taskSalary"],
   additionalProperties: false,
