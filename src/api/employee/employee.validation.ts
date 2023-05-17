6bce0f7f6c8ddcd74232c0b9d51f7b03eed55f8a
Omar Saeed <omar.ahmed.saeed.1999@gmail.com>
2023-05-17 03:56:59 +0300

    fix: add description to employee json schemas

--- src/api/employee/employee.validation.ts
+++ src/api/employee/employee.validation.ts
@@ -1,13 +1,40 @@
 const employeeSchema = {
+  $schema: "http://json-schema.org/draft-07/schema#",
+  title: "Employee",
   type: "object",
   properties: {
-    name: { type: "string" },
-    email: { type: "string", format: "email" },
-    password: { type: "string" },
-    joinDate: { type: "string" },
-    birthDate: { type: "string" },
-    role: { type: "string", enum: ["SUPER_ADMIN", "HR", "DEVELOPER"] },
-    profilePicture: { type: ["string", "null"] },
+    name: {
+      type: "string",
+      description: "The name of the employee",
+    },
+    email: {
+      type: "string",
+      format: "email",
+      description: "The email address of the employee",
+    },
+    password: {
+      type: "string",
+      description: "The password of the employee",
+    },
+    joinDate: {
+      type: "string",
+      format: "date",
+      description: "The date when the employee joined",
+    },
+    birthDate: {
+      type: "string",
+      format: "date",
+      description: "The birth date of the employee",
+    },
+    role: {
+      type: "string",
+      enum: ["SUPER_ADMIN", "HR", "DEVELOPER"],
+      description: "The role of the employee",
+    },
+    profilePicture: {
+      type: ["string", "null"],
+      description: "The URL of the employee's profile picture",
+    },
   },
   required: ["name", "email", "password", "joinDate", "birthDate", "role"],
   additionalProperties: false,
@@ -16,8 +43,14 @@ const employeeSchema = {
 const salaryQuerySchema = {
   type: "object",
   properties: {
-    month: { type: "string" },
-    year: { type: "string" },
+    month: {
+      type: "string",
+      description: "The month for which salary is queried",
+    },
+    year: {
+      type: "string",
+      description: "The year for which salary is queried",
+    },
   },
   required: ["month", "year"],
   additionalProperties: false,
